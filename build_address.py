baabc4a4d34798045d7fc378131c790714572f10
Michael Byrne <feomike@users.noreply.github.com>
2015-01-02 12:21:17 -0500

    interim update
    this update is a work in progress trying to eliminate single cursors which increase processing time (e.g. efficiency's).  however, the full scope of the update (like removing all the get_azimuth and fully switching to geojson output) are still coming.

--- build_address.py
+++ build_address.py
@@ -53,7 +53,7 @@ myPort = "54321"
 myUser = "postgres"
 db = "feomike"
 schema = "census"
-theTBL = "tl_2014_44005_addrfeat" #"tl_2014_44005_addrfeat" #"powel" #"working" 
+theTBL = "powel" #"tl_2014_05119_addrfeat" #"tl_2014_44005_addrfeat" #"powel" #"working" 
 idFld = "tlid"
 in_Prj = "4269"
 #http://spatialreference.org/ref/epsg/3786/
@@ -113,10 +113,9 @@ def get_az(myID):
 	mySQL = mySQL + "." + theTBL + " WHERE gid = "
 	mySQL = mySQL + str(myID) + "; " 
 	#execute the query, get the returned point 
-	myCur = conn.cursor()
-	myCur.execute(mySQL)
-	if myCur.rowcount == 1:
-		myrow = myCur.fetchone()
+	someCur.execute(mySQL)
+	if someCur.rowcount == 1:
+		myrow = someCur.fetchone()
 		theSX = myrow[0]
 		theSY = myrow[1]
 		theEX = myrow[2]
@@ -128,18 +127,36 @@ def get_az(myID):
 		theEY = 90
 	#next get the azimuth of the line from these two points
 	mySQL = "SELECT degrees(ST_Azimuth(ST_GeomFromText(' "
-	mySQL = mySQL + "POINT( " + str(theSX) + " " + str(theSY) + ")',4269),"
+	mySQL = mySQL + "POINT( " + str(theSX) + " " + str(theSY) + ")'," + in_Prj + "),"
 	mySQL = mySQL + "ST_GeomFromText('POINT("
-	mySQL = mySQL + str(theEX) + " " + str(theEY) + ")',4269) )); "
+	mySQL = mySQL + str(theEX) + " " + str(theEY) + ")'," + in_Prj + ") )); "
 	#execute the query, get the returned direction 
-	myCur.execute(mySQL)
-	if myCur.rowcount == 1:
-		myrow = myCur.fetchone()
+	someCur.execute(mySQL)
+	if someCur.rowcount == 1:
+		myrow = someCur.fetchone()
 		myDir = myrow[0]
 		#make sure the myDir is set to something
 		if myDir == None:
 			myDir = 180
-	myCur.close()	
+	return(myDir)
+
+#return the direction of that line for proper offsetting
+#arguments:
+#- myID is the unique ID of the row you are operating on
+def get_az_xy(theSX,theSY,theEX,theEY ):
+	#next get the azimuth of the line from these two points
+	mySQL = "SELECT degrees(ST_Azimuth(ST_GeomFromText(' "
+	mySQL = mySQL + "POINT( " + str(theSX) + " " + str(theSY) + ")'," + in_Prj + "),"
+	mySQL = mySQL + "ST_GeomFromText('POINT("
+	mySQL = mySQL + str(theEX) + " " + str(theEY) + ")'," + in_Prj + ") )); "
+	#execute the query, get the returned direction 
+	someCur.execute(mySQL)
+	if someCur.rowcount == 1:
+		myrow = someCur.fetchone()
+		myDir = myrow[0]
+		#make sure the myDir is set to something
+		if myDir == None:
+			myDir = 180	
 	return(myDir)
 
 #make an address from the values of street, zip and zip+4
@@ -154,6 +171,10 @@ def ret_add(myst, myzip, myz4, mycity, myST):
 	#into a table, and sql needs the single quote escaped
 	if "'" in myst:
 		myst = myst.replace("'", "''")
+	if myzip == None:
+		myzip = " " 
+	if mycity == None:
+		mycity = " "
 	if myz4 is None:
 		myadd = myst + " " + mycity + " " + myST + " " + myzip
 	else:
@@ -175,7 +196,7 @@ def ret_add(myst, myzip, myz4, mycity, myST):
 #-myadd is the full address minus the house number (e.g. Main St Somecity, CA 95616
 #-side is left of right; the side of the street to grow on
 #-myDir is the azimuth direction of the street in degrees 0 - 360
-def mk_pts(num, myID, house, myadd, side, myAz, myGID, myDir):
+def mk_pts(num, myID, house, myadd, side, myAz, myGID, myDir, myFirst):
 	#this section intends to come up with the percentage along the line to generate 
 	#a point.  the percent is +1 of the number of addresses to do along that line
 	#(e.g. if n=3, points should be generated every 25%)
@@ -186,6 +207,7 @@ def mk_pts(num, myID, house, myadd, side, myAz, myGID, myDir):
 	myHouse = int(house)
 	#set up a cursor for this one
 	myCur = conn.cursor()
+	theend = False
 	while myCnt < (num + 1):
 		#format a sql statement to obtain the point as a percentage along the line
 		#return the values for the point, so you can insert them as 
@@ -219,7 +241,14 @@ def mk_pts(num, myID, house, myadd, side, myAz, myGID, myDir):
 		thePt = offset_pt(theX, theY, side, myAz)
 		newX = thePt[0]
 		newY = thePt[1]
-		insert_pt(myID, str(myHouse) + " " + myadd, newX, newY)
+		if myFirst:
+			if myCnt > 1:
+				f.write(",\n")
+		else:
+			f.write(",\n")
+		myFirst = False
+		#insert_pt(myID, str(myHouse) + " " + myadd, newX, newY)
+		write_geoJson(myGID, myID, str(myHouse) + " " + myadd, newX, newY)
 		#myHouse always gets incremented by two
 		if myDir == "forward":
 			myHouse = myHouse + 2
@@ -233,6 +262,11 @@ def mk_pts(num, myID, house, myadd, side, myAz, myGID, myDir):
 #side that these addresses should be on.  this function uses trigonometry to move a 
 #a delta x and and delta y.  
 #then return new X and Y as a list
+#arguments:
+#-myX is the X value of the input point
+#-myY is the Y value of the input point
+#-mySide is the side of the line (left or right)
+#-myAz is the azimuth direction of the line (0-360)
 def offset_pt(myX, myY, mySide, myAz):
 	#we are offsetting by 5 meters
 	theOffset = 10
@@ -252,6 +286,17 @@ def offset_pt(myX, myY, mySide, myAz):
 	#triangle, so we use cosine of that angle to acquire this distance.
 	newX = myX + (math.sin(math.radians(newDir)))*theOffset
 	newY = myY + (math.cos(math.radians(newDir)))*theOffset
+	#transform the xy back to 4269, and return, then retest
+	#all of newport county took - start time: Thu Jan  1 16:47:09 2015
+     #                                   going to be operating on this many segments: 7284
+	#                             end time: Thu Jan  1 16:52:05 2015
+	mySQL = "SELECT  " 
+	mySQL = mySQL + "st_x(st_transform(ST_GeomFromText('POINT(" + str(newX) + " " + str(newY) + ")', " + eqd_prj + "), " + in_Prj + " )), "
+	mySQL = mySQL + "st_y(st_transform(ST_GeomFromText('POINT(" + str(newX) + " " + str(newY) + ")', " + eqd_prj + "), " + in_Prj + " )); "	
+	someCur.execute(mySQL)
+	myrow = someCur.fetchone()
+	newX = myrow[0]
+	newY = myrow[1]	
 	return ([newX, newY])
 	
 #insert the new point as a row
@@ -264,36 +309,69 @@ def insert_pt(myID, myadd, myX, myY):
 	mySQL = mySQL + "); COMMIT;"
 	updCur.execute(mySQL)
 
+#instead of writing a db, lets write to a geoJson file
+def write_geoJson(myGID, myTLID, myAdd, myX, myY):
+	#example is { "type": "Feature", "id": 1, "properties": 
+	#example    { "gid": 1, "tlid": 47349507.000000, "address": "48 Powel Ave Newport RI 02840" }, 
+	#example            "geometry": { "type": "Point", "coordinates": [ -71.304186, 41.495393 ] } }
+     myjson = "{ \"type\": \"Feature\", \"id\": " + str(myGID) + ", \"properties\": { "
+     myjson = myjson + "\"tlid\": " + str(myTLID) + ", \"address\": \"" + myAdd + "\" },"
+     myjson = myjson + "\"geometry\":{\"type\": \"Point\", \"coordinates\": [ "
+     myjson = myjson + str(myX) + ", " + str(myY) + "] } }"
+     f.write(myjson + "\n")
+
 try:
 	#set up the connection to the database
 	myConn = "dbname=" + db + " host=" + myHost + " port=" + myPort + " user=" + myUser
 	conn = psycopg2.connect(myConn)
-	theCur = conn.cursor()
-	updCur = conn.cursor()
+	theCur = conn.cursor() #used for loop queries
+	updCur = conn.cursor() #used for updating rows
+	someCur = conn.cursor() #used for single select statements
 	#make new table to insert, based on the input table name
-	mk_Tbl(theTBL, theCur)
+	mk_Tbl(theTBL, someCur)
 	#get the total number of records to go through
+	#azimuth is a tricky one b/c the input is a multi-line string, not a line string
 	theSQL = "SELECT tlid, lfromhn, ltohn, rfromhn, rtohn, fullname, zipl, zipr, "
-	theSQL = theSQL + "plus4l, plus4r, city, st_abbr, gid "
+	theSQL = theSQL + "plus4l, plus4r, city, st_abbr, gid, "
+	theSQL = theSQL + "degrees(ST_Azimuth("
+	theSQL = theSQL + "ST_StartPoint(ST_GeometryN(ST_Multi(geom),1)),"
+	theSQL = theSQL + "ST_EndPoint(ST_GeometryN(ST_Multi(geom),1))"
+	theSQL = theSQL + ")) as azimuth"
+	#here insert the start end point of the line, to only make the query once
+	#you need this for getAZ
 	theSQL = theSQL + " from " + schema + "." + theTBL 
 	theSQL = theSQL + ";"
 	theCur.execute(theSQL)
 	thenum = theCur.rowcount
 	print "     going to be operating on this many segments: " + str(thenum)
 	counter = 1
+	first = True 
+	myFile = "mbadd_" + theTBL + ".geojson"
+	f = open(myFile,'w')
+	myStr = "{\"type\": \"FeatureCollection\", \"features\":[ "
+	f.write (myStr + "\n")
 	for row in theCur:
 		#get all the necessary field data out of the record for the cursor
 		#values are as follows
 		#0:tlid, 1:lfrom, 2:lto, 3:rfrom, 4:rto, 5:st, 6:lzip, 
 		#7:rzip, 8:lzip4, 9:lzip4, 10:city, 11:st_abbr, 12:gid
-		print "working on tlid: " + str(row[0])
+		#13:azimuth
+#		print "working on tlid: " + str(row[0])
 		doL = 0
 		lfrom = row[1]
 		lto = row[2]
 		rfrom = row[3]
 		rto = row[4]
+		azimuth = row[13]
+		#for each row, get the direction of the line; pass in the gid
+		#azimuth can be faster if we add the get select onto the loop query + 
+		#put it under a conditional for the l/r from/to being non-null
+#		if (lfrom is not None and lto is not None) or (rfrom is not None and rto is not None):
+#			azimuth = get_az_xy(row[13], row[14], row[15], row[16])
+#			print "function azimuth is: " + str(azimuth)
 		#make sure there is a value in the left address field
 		if lto is not None and lfrom is not None:
+			add = ret_add(row[5], row[6], row[8], row[10], row[11])
 			#make sure the left address values are only numbers
 			if isNum(lfrom) and isNum(lto):
 				lCnt = round(float(int(lto)- int(lfrom) + 1) / float(2))
@@ -304,11 +382,18 @@ try:
 					ldir = "reverse"
 					lCnt = abs(lCnt)
 				doL = 1
+			#for the left side, create the desired number of address points
+			#only do this if there is an address range (values in both from and to fields)
+			if doL == 1:
+				mk_pts(lCnt, row[0], row[1], add, "left", azimuth, row[12], ldir,first)
+				first = False
+
 		doR = 0
 		#make sure there is a value in the left address field
 		if rto is not None and rfrom is not None:
 			#make sure the left address values are single numbers
 			#make sure the left address values are only numbers
+			add = ret_add(row[5], row[7], row[9	], row[10], row[11])
 			if isNum(rfrom) and isNum(rto):
 				rCnt = round(float(int(rto)- int(rfrom) + 1) / float(2))
 				#if from > to, then need to go in reverse
@@ -318,17 +403,15 @@ try:
 					rdir = "reverse"
 					rCnt = abs(rCnt)
 				doR = 1
-		#for each row, get the direction of the line; pass in the gid
-		azimuth = get_az(row[12])
-		#for each side, create the desired number of address points
-		#only do this if there is an address range (values in both from and to fields)
-		if row[1] is not None and row[2] is not None:
-			#return the direction of the line
-			add = ret_add(row[5], row[6], row[8], row[10], row[11])
-			if doL == 1:
-				mk_pts(lCnt, row[0], row[1], add, "left", azimuth, row[12], ldir)
+			#for the left side, create the desired number of address points
+			#only do this if there is an address range (values in both from and to fields)
 			if doR == 1:
-				mk_pts(rCnt, row[0], row[3], add, "right", azimuth, row[12], rdir)
+				mk_pts(rCnt, row[0], row[3], add, "right", azimuth, row[12], rdir,first)
+				first = False
+		counter = counter + 1
+		
+	f.write("]}" + "\n")
+	f.close()
 	updCur.close()
 	theCur.close()
 	del updCur, theCur
